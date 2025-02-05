e61d408d4fb307c1db701732ab9ba6c52dab00e2
xyafedev <arifeenafif24@gmail.com>
2025-02-05 20:12:31 +0700

    update form login & reg

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,22 @@
-"use client"; // Tambahkan ini di bagian atas file
-
+"use client";
 import { useState } from "react";
-import { useRouter } from "next/navigation"; // Perubahan di sini
+import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabase/client";
 import Link from "next/link";
+import Image from "next/image";
 import { toast } from "react-toastify";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const router = useRouter(); // Menggunakan useRouter dari next/navigation
+  const [showPassword, setShowPassword] = useState(false); // State untuk toggle visibility password
+  const router = useRouter();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
+    console.log("Logging in with:", { email, password });
+
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
@@ -37,9 +40,13 @@ export default function Login() {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <form
         onSubmit={handleLogin}
-        className="bg-white p-8 rounded shadow-md w-96"
+        className="bg-white p-8 rounded shadow-md w-4/12"
       >
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <h2 className="text-2xl font-bold">Login</h2>
+        <p className="mb-4 mt-2 text-gray-600 text-sm">
+          Silahkan Masukkan E-mail dan Password untuk melanjutkan ke dalam
+          aplikasi
+        </p>
         {error && <p className="text-red-500 mb-4">{error}</p>}
         <div className="mb-4">
           <label className="block text-gray-700">Email</label>
@@ -47,31 +54,58 @@ export default function Login() {
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-3 py-2 border rounded"
+            className="block w-full rounded-md border-2 border-gray-300 shadow-sm outline-none p-2 focus:border-gray-400 focus:ring-gray-400"
             required
+            placeholder="Masukkan email"
           />
         </div>
-        <div className="mb-4">
+        <div className="mb-4 relative">
           <label className="block text-gray-700">Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-3 py-2 border rounded"
-            required
-          />
+          <div className="flex justify-between rounded-md border-2 border-gray-300 shadow-sm outline-none p-2 focus:border-gray-400 focus:ring-gray-400">
+            <input
+              type={showPassword ? "text" : "password"} // Toggle visibility password
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="block w-full outline-none"
+              required
+              placeholder="Masukkan password"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)} // Toggle visibility
+              className="flex items-center text-gray-500 hover:text-gray-700"
+            >
+              {showPassword ? (
+                <Image
+                  src={"/images/eye-open.svg"}
+                  alt="Show Password"
+                  width={25}
+                  height={25}
+                />
+              ) : (
+                <Image
+                  src={"/images/eye-closed.svg"}
+                  alt="Hide Password"
+                  width={25}
+                  height={25}
+                />
+              )}
+            </button>
+          </div>
         </div>
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded"
+          className="w-full bg-green-500 text-white py-2 rounded"
         >
           Login
         </button>
-
         <p className="mt-4 text-center">
-          Dont have an account?{" "}
-          <Link href="/register" className="text-blue-500">
-            Register here
+          Tidak punya akun?{" "}
+          <Link
+            href="/register"
+            className="text-green-500 underline hover:text-green-600"
+          >
+            Daftar akun disini
           </Link>
         </p>
       </form>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,23 +1,34 @@
 "use client";
-
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabase/client";
 import { toast } from "react-toastify";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState(""); // State untuk konfirmasi password
   const [fullName, setFullName] = useState("");
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // State untuk toggle visibility password
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false); // State untuk toggle visibility konfirmasi password
   const router = useRouter();
 
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    // Validasi konfirmasi password
+    if (password !== confirmPassword) {
+      setError("Password dan Konfirmasi Password tidak cocok.");
+      toast.error("Password dan Konfirmasi Password tidak cocok.");
+      return;
+    }
+
     console.log("Registering user with:", { email, password, fullName });
 
-    // Set role default sebagai "user_santri"
-    const { data, error } = await supabase.auth.signUp({
+    const { data, error: signUpError } = await supabase.auth.signUp({
       email,
       password,
       options: {
@@ -25,14 +36,14 @@ export default function Register() {
       },
     });
 
-    if (error) {
-      console.error("Error during registration:", error.message);
-      setError(error.message);
+    if (signUpError) {
+      console.error("Error during registration:", signUpError.message);
+      setError(signUpError.message);
       toast.error("Registrasi gagal. Silakan coba lagi.");
     } else {
       console.log("Registration successful. Data:", data);
       toast.success("Registrasi berhasil! Silakan login.");
-      router.push("/"); // Redirect ke halaman utama santri
+      router.push("/");
     }
   };
 
@@ -40,18 +51,23 @@ export default function Register() {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <form
         onSubmit={handleRegister}
-        className="bg-white p-8 rounded shadow-md w-96"
+        className="bg-white p-8 rounded shadow-md w-4/12"
       >
-        <h2 className="text-2xl font-bold mb-4">Register</h2>
+        <h2 className="text-2xl font-bold ">Register</h2>
+        <p className="mb-4 mt-2 text-gray-600 text-sm">
+          Silahkan Isi Nama Lengkap, E-mail, Password, dan Konfirmasi Password
+          untuk membuat akun
+        </p>
         {error && <p className="text-red-500 mb-4">{error}</p>}
         <div className="mb-4">
-          <label className="block text-gray-700">Full Name</label>
+          <label className="block text-gray-700">Nama Lengkap</label>
           <input
             type="text"
             value={fullName}
             onChange={(e) => setFullName(e.target.value)}
-            className="w-full px-3 py-2 border rounded"
+            className="block w-full rounded-md border-2 border-gray-300 shadow-sm outline-none p-2 focus:border-gray-400 focus:ring-gray-400"
             required
+            placeholder="Masukkan nama lengkap"
           />
         </div>
         <div className="mb-4">
@@ -60,19 +76,78 @@ export default function Register() {
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-3 py-2 border rounded"
+            className="block w-full rounded-md border-2 border-gray-300 shadow-sm outline-none p-2 focus:border-gray-400 focus:ring-gray-400"
             required
+            placeholder="Masukkan email"
           />
         </div>
-        <div className="mb-4">
+        <div className="mb-4 relative">
           <label className="block text-gray-700">Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-3 py-2 border rounded"
-            required
-          />
+          <div className="flex justify-between rounded-md border-2 border-gray-300 shadow-sm outline-none p-2 focus:border-gray-400 focus:ring-gray-400">
+            <input
+              type={showPassword ? "text" : "password"} // Toggle visibility password
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="block w-full outline-none"
+              required
+              placeholder="Masukkan password"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)} // Toggle visibility
+              className="flex items-center text-gray-500 hover:text-gray-700"
+            >
+              {showPassword ? (
+                <Image
+                  src={"/images/eye-open.svg"}
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+              ) : (
+                <Image
+                  src={"/images/eye-closed.svg"}
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+              )}
+            </button>
+          </div>
+        </div>
+        <div className="mb-4 relative">
+          <label className="block text-gray-700">Konfirmasi Password</label>
+          <div className="flex justify-between rounded-md border-2 border-gray-300 shadow-sm outline-none p-2 focus:border-gray-400 focus:ring-gray-400">
+            <input
+              type={showConfirmPassword ? "text" : "password"} // Toggle visibility konfirmasi password
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="block w-full outline-none"
+              required
+              placeholder="Masukkan ulang password"
+            />
+            <button
+              type="button"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)} // Toggle visibility
+              className="flex items-center text-gray-500 hover:text-gray-700"
+            >
+              {showConfirmPassword ? (
+                <Image
+                  src={"/images/eye-open.svg"}
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+              ) : (
+                <Image
+                  src={"/images/eye-closed.svg"}
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+              )}
+            </button>
+          </div>
         </div>
         <button
           type="submit"
@@ -80,6 +155,15 @@ export default function Register() {
         >
           Register
         </button>
+        <p className="mt-4 text-center">
+          Sudah punya akun?{" "}
+          <Link
+            href="/login"
+            className="text-green-500 underline hover:text-green-600"
+          >
+            Login akun disini
+          </Link>
+        </p>
       </form>
     </div>
   );
